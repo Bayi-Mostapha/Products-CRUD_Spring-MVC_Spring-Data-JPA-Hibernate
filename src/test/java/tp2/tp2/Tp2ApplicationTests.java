a20b4327d5f1a3a1fcb4522b66c10023f4b38163
Bayi-Mostapha <bayimostapha@gmail.com>
2026-03-09 09:34:43 +0000

    initial commit

---
+++ src/test/java/tp2/tp2/Tp2ApplicationTests.java
@@ -0,0 +1,13 @@
+package tp2.tp2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Tp2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
