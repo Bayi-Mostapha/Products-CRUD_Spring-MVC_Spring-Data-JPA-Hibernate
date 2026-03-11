6260d5a8f755006d2b2ece671889b19431b3bf82
Bayi-Mostapha <bayimostapha@gmail.com>
2026-03-11 23:42:05 +0000

    user roles + permissions

--- src/main/java/tp2/tp2/web/ProductController.java
+++ src/main/java/tp2/tp2/web/ProductController.java
@@ -4,18 +4,21 @@ import java.util.List;
 import tp2.tp2.repository.ProductRepository;
 import tp2.tp2.entities.Product;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import jakarta.validation.Valid;
 
 @Controller
 public class ProductController {
     @Autowired
     private ProductRepository productRepository;
 
-    @GetMapping("/index")
+    @GetMapping("/user/index")
     public String index(Model model) {
         List<Product> products = productRepository.findAll();
         model.addAttribute("productList", products);
@@ -24,18 +27,25 @@ public class ProductController {
 
     @GetMapping("/")
     public String home() {
-        return "redirect:/index";
+        return "redirect:/user/index";
     }
 
-    @GetMapping("/delete")
+    @GetMapping("/admin/delete")
     public String delete(@RequestParam(name = "id") Long id) {
         productRepository.deleteById(id);
-        return "redirect:/index";
+        return "redirect:/user/index";
     }
 
-    @GetMapping("/newProduct")
+    @GetMapping("/admin/newProduct")
     public String newProduct(Model model) {
         model.addAttribute("product", new Product());
         return "new-product";
+    }
+
+    @PostMapping("/admin/saveProduct")
+    public String saveProduct(@Valid Product product, BindingResult bindingResult, Model model) {
+        if(bindingResult.hasErrors()) return "new-product";
+        productRepository.save(product);
+        return "redirect:/admin/newProduct";
     }
 }
